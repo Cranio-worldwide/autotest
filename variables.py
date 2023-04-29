3c076f77d39056d275ea199565bf5a737d5dcab6
Viacheslav Shchukin <gendalf1984@gmail.com>
2023-04-29 21:07:38 +0300

    initial commit

---
+++ variables.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class httpHeaders():
+    JSONHEADERS = [('User-Agent',
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.3.886 Yowser/2.5 Safari/537.36"),
+           ('Accept', 'application/json'),
+           ('Accept-Language', 'ru,en;q=0.9'),
+           ('Connection', 'keep-alive'),
+           ('Content-Type', 'application/json')]
+
+
+class httpLinks():
+    DOMAIN = "http://10.10.1.2:8000"
+    AUTHTOKEN = "/api/v1/auth/token/"
