77221a63aecc6b0338d3d666290b9fcd5b8fae6e
Viacheslav Shchukin <gendalf1984@gmail.com>
2023-05-18 19:23:33 +0300

    Переписан хедер. Теперь ожидаются ответы только на английском

--- variables.py
+++ variables.py
@@ -10,15 +10,14 @@ class httpHeaders:
                    ('Accept-Language', 'ru,en;q=0.9'),
                    ('Connection', 'keep-alive'),
                    ('Content-Type', 'application/json')]
-    JSON_HEADERS = {'User-Agent':
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/"
-                    "537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 "
-                    "YaBrowser/22.9.3.886 Yowser/2.5 Safari/537.36",
-                    'Accept': 'application/json',
-                    'Accept-Language': 'ru,en;q=0.9',
-                    'Connection': 'keep-alive',
-                    'Content-Type': 'application/json'}
-    ACCEPTHEADERS = {'User-Agent':
+    JSON_HEADERS = {"User-Agent":
+                    "python autotest script v=0.1",
+                    "Accept": "*/*",
+                    "Accept-Language": "en;q=0.9",
+                    "Connection": "keep-alive",
+                    "Content-Type": "application/json",
+                    "Accept-Encoding":"gzip, deflate, br"}
+    ACCEPTHEADERS = {"User-Agent":
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/"
                     "537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 "
                     "YaBrowser/22.9.3.886 Yowser/2.5 Safari/537.36"
@@ -26,7 +25,7 @@ class httpHeaders:
 
 
 class httpLinks:
-    DOMAIN = "https://backend.princeofprocrastination.art"
+    DOMAIN = "http://10.10.1.2:8000"
     AUTHREGISTER = "/api/v1/auth/register/"
     AUTHTOKEN = "/api/v1/auth/token/"
     AUTHREFRESH = "/api/v1/auth/token/refresh/"
